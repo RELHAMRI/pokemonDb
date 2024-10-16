5439485f0ace07dee73e0e7ba41efb349e08b891
RELHAMRI <elh.radouane@gmail.com>
2024-10-16 11:08:45 +0200

    afficher les pokemon

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,11 +3,16 @@
 namespace App\Controllers;
 
 use App\Utils\AbstractController;
+use App\Models\Pokemon;
 
 class HomeController extends AbstractController
 {
     public function index()
     {
+        $pokemon = new Pokemon(null, null, null, null, null);
+        $allPokemon = $pokemon->getAll();
+        
         require_once(__DIR__ . '/../Views/home.view.php');
     }
+
 }
--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -2,7 +2,25 @@
 require_once(__DIR__ . "/partials/head.php");
 ?>
 
-<div class="home">
-    <h1>Bienvenue</h1>
+<div class="container">
+    <h1>Bienvenue sur la page </h1>
+    <?php
+    foreach ($allPokemon as $pokemon) {
+    ?>
+        <div class="flex-container">
+            <div class="card <?= $pokemon->getType() ?>">
+                <h2><?= $pokemon->getName() ?></h2>
+                <p>Type : <?= $pokemon->getType() ?></p>
+                <p>Niveau : <?= $pokemon->getLevel() ?></p>
+                <button class="voirPlus">Voir plus</button>
+                <p class="description hidden"><?= $pokemon->getDescription() ?></p>
+                <button class="voirMoins hidden">Réduire</button>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
 </div>
 
+<?php
+require_once (__DIR__ . '/partials/footer.php');
